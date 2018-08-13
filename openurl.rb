13d90a1477bed6c9dcb1da14c6afb4c82c20b8c0
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2018-08-13 14:31:33 +0100

    Update openurl.rb

--- openurl.rb
+++ openurl.rb
@@ -1,5 +1,8 @@
 require 'selenium-webdriver'
 driver=Selenium::WebDriver.for:firefox
+
+#enter URL you wish to navigate to.
+
 driver.navigate.to "https://www.google.co.uk/"
 driver.manage.window.maximize
 
