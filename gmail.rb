f5cc736d7bd8c25fb92a03262a4d9a03d91c9df0
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2018-08-21 13:39:03 +0100

    Gmail
    The script opens Google and navigates to Gmail from the Gmail link on the Homepage.

---
+++ gmail.rb
@@ -0,0 +1,18 @@
+require 'selenium-webdriver'
+driver=Selenium::WebDriver.for:firefox
+
+#enter URL you wish to navigate to.
+
+driver.navigate.to "https://www.google.co.uk/"
+driver.manage.window.maximize
+
+# sleep = 5 sec
+sleep (2)
+
+# gmail
+driver.find_element(:link, "Gmail").click()
+
+puts "Test Passed"
+
+#Quitting the browser
+driver.quit
