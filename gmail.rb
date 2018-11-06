599d48e6c3f0fdf8b7f18b0d52bb30bf0121ee6d
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2018-11-06 12:58:37 +0000

    Update gmail.rb

--- gmail.rb
+++ gmail.rb
@@ -7,11 +7,13 @@ driver.navigate.to "https://www.google.co.uk/"
 driver.manage.window.maximize
 
 # sleep = 5 sec
-sleep (2)
+sleep (5)
 
 # Click the Gmail link from google.co.uk web page
 
 driver.find_element(:link, "Gmail").click()
+
+#If test passes. Print success message to the terminal.
 
 puts "Test Passed"
 
