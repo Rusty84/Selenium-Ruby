6015ff657f20b6a1b5ab5e93598b6f5d6bd52723
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2018-11-06 13:23:41 +0000

    Create gmailfail.rb
    When an element is not found within the 15-second wait. The test will timeout and fail.

---
+++ gmailfail.rb
@@ -0,0 +1,27 @@
+require 'selenium-webdriver'
+driver=Selenium::WebDriver.for:firefox
+
+#enter URL you wish to navigate to.
+
+driver.navigate.to "https://www.google.co.uk/"
+driver.manage.window.maximize
+
+# create a wait with a timeout of 15 seconds
+
+wait = Selenium::WebDriver::Wait.new(timeout: 15)
+
+
+# Click the Gmail link from google.co.uk web page
+
+link = wait.until {
+    element = driver.find_element(:link, "fdjk")
+    element if element.displayed?
+}
+link.click()
+	
+#If test passes. Print success to the terminal.
+
+puts "Test Passed"
+
+#Quitting the browser
+driver.quit
