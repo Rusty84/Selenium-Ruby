89f8b22974da1b7f28fbf1c2c1b2bb2d0905b031
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2018-11-06 13:27:19 +0000

    Update openurl.rb
    Removed sleep of 5 seconds.

--- openurl.rb
+++ openurl.rb
@@ -6,9 +6,6 @@ driver=Selenium::WebDriver.for:firefox
 driver.navigate.to "https://www.google.co.uk/"
 driver.manage.window.maximize
 
-# sleep = 5 sec
-sleep (5)
-
 puts "Test Passed"
 
 #Quitting the browser
