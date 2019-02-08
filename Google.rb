c3835e5704e5b73b62ea277125a5283779fe9532
Russell Morley <42317844+Rusty84@users.noreply.github.com>
2019-02-08 13:53:46 +0000

    Add files via upload

---
+++ Google.rb
@@ -0,0 +1,26 @@
+require "selenium-webdriver"
+
+ARGV.each do|a|
+  puts "Argument: #{a}" "Argument: #{a}"
+end
+
+#Firefox browser instantiation
+driver = Selenium::WebDriver.for :firefox
+
+#Loading the Experian URL
+driver.navigate.to "http://www.google.co.uk"
+
+# sleep = 2 sec
+sleep (2)
+
+# create a wait with a timeout of 15
+wait = Selenium::WebDriver::Wait.new(timeout: 15)
+
+input = wait.until {
+    element = driver.find_element(:name, "q")
+    element if element.displayed?
+}
+input.send_keys(ARGV[0])
+input.submit()
+
+
